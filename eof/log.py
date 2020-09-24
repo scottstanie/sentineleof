de457120060aa5a5cca629df10aa5cd07191af78
scott <scott.stanie@gmail.com>
2020-09-24 17:13:46 -0500

    move logger to new file

---
+++ eof/log.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def _set_logger_handler(level="INFO"):
+    logger.setLevel(level)
+    h = logging.StreamHandler()
+    h.setLevel(level)
+    format_ = "[%(asctime)s] [%(levelname)s %(filename)s] %(message)s"
+    fmt = logging.Formatter(format_, datefmt="%m/%d %H:%M:%S")
+    h.setFormatter(fmt)
+    logger.addHandler(h)
+
+
+logger = logging.Logger("sentineleof")
+_set_logger_handler()
