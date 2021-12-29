65fd963ce013bb1aa50f7b91271079ad32f91ca5
Antonio Valentino <antonio.valentino@tiscali.it>
2021-12-29 10:25:35 +0100

    Update classifiers to add Python 3.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: C",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Science/Research",
