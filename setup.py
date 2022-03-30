6c764fbaee2b295f760f8b77721421cafa0136ed
scott <scott.stanie@gmail.com>
2022-03-30 09:54:11 -0500

    wrong email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="sentineleof",
     version="0.6.4",
     author="Scott Staniewicz",
-    author_email="scott.stanie@utexas.com",
+    author_email="scott.stanie@gmail.com",
     description="Download precise orbit files for Sentinel 1 products",
     long_description=long_description,
     long_description_content_type="text/markdown",
