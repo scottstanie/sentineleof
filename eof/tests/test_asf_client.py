0ffb43169cf3fab70bb79f931c474c24cc763584
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-10-31 18:52:46 -0400

    more cassettes for asf

--- eof/tests/test_asf_client.py
+++ eof/tests/test_asf_client.py
@@ -4,18 +4,22 @@ import pytest
 
 from eof.asf_client import ASFClient
 
+# pytest --record-mode=all
 
-# pytest --record-mode=once test_network.py
+
 @pytest.mark.vcr
 def test_asf_client():
     ASFClient()
 
 
 @pytest.mark.vcr
-def test_asf_client_download():
+def test_asf_client_download(tmp_path):
+    cache_dir = tmp_path / "sentineleof"
+    cache_dir.mkdir()
+
     dt = datetime.datetime(2020, 1, 1)
     mission = "S1A"
-    asfclient = ASFClient()
+    asfclient = ASFClient(cache_dir=(tmp_path / "sentineleof"))
     urls = asfclient.get_download_urls([dt], [mission], orbit_type="precise")
     expected = "https://s1qc.asf.alaska.edu/aux_poeorb/S1A_OPER_AUX_POEORB_OPOD_20210315T155112_V20191230T225942_20200101T005942.EOF"  # noqa
     assert urls == [expected]
@@ -25,6 +29,7 @@ def test_asf_client_download():
 def test_asf_full_url_list(tmp_path):
     cache_dir = tmp_path / "sentineleof"
     cache_dir.mkdir()
+
     asfclient = ASFClient(cache_dir=(tmp_path / "sentineleof"))
     urls = asfclient.get_full_eof_list()
     assert len(urls) > 0
