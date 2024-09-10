d3551ad502790aaa2088c677b424682c5de3f3eb
Luc Hermitte <luc.hermitte@csgroup.eu>
2024-09-10 15:27:04 +0200

    Test ASF-query of all EOFs intersecting time range

--- eof/tests/test_asf_client.py
+++ eof/tests/test_asf_client.py
@@ -56,6 +56,35 @@ def test_asf_full_url_list(tmp_path, asfclient: ASFClient):
 
 
 @pytest.mark.vcr
+def test_asf_query_orbit_files_by_dt_range(asfclient: ASFClient):
+    dt1 = datetime.datetime(2020, 1, 1)   # 00:00:00
+    dt2 = datetime.datetime(2020, 1, 12)  # 00:00:00
+    mission = "S1A"
+    # Restituted seems to fail for old dates...
+    # Need to look into sentinelsat, or if ESA has just stopped allowing it
+    results = asfclient.query_orbit_files_by_dt_range(dt1, dt2, [mission], orbit_type=OrbitType.precise)
+    assert len(results) == 12  # 12 files intersect from 20200101T00:00:00 to 20200112T00:00:00
+    for r in results:
+        assert r.stop_time >= dt1
+        assert r.start_time <= dt2
+    r = results[0]
+    assert r.filename == "S1A_OPER_AUX_POEORB_OPOD_20210315T155112_V20191230T225942_20200101T005942.EOF"
+
+
+def test_asf_query_orbit_urls_by_dt_range(asfclient: ASFClient):
+    dt1 = datetime.datetime(2020, 1, 1)   # 00:00:00
+    dt2 = datetime.datetime(2020, 1, 12)  # 00:00:00
+    mission = "S1A"
+    # Restituted seems to fail for old dates...
+    # Need to look into sentinelsat, or if ESA has just stopped allowing it
+    results = asfclient.query_orbits_by_dt_range(dt1, dt2, [mission], orbit_type=OrbitType.precise)
+    assert len(results) == 12  # 12 files intersect from 20200101T00:00:00 to 20200112T00:00:00
+
+    expected = "https://s1qc.asf.alaska.edu/aux_poeorb/S1A_OPER_AUX_POEORB_OPOD_20210315T155112_V20191230T225942_20200101T005942.EOF"  # noqa
+    assert results[0] == expected
+
+
+@pytest.mark.vcr
 def test_asf_client_download(asfclient: ASFClient):
     dt = datetime.datetime(2020, 1, 1)
     mission = "S1A"
